76e5af5a43bb94af364a810c056ca14dacc4b556
dpera005Xamk <dpera005@edu.xamk.fi>
2023-01-29 18:05:27 +0200

    firebase connect added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { firebaseConfig } from './config.js';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
